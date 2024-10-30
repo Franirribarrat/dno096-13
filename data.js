e3753ddfb65a03aa6a32ede319a9db9f49fd73c6
Franirribarrat <franirribarrat@uc.cl>
2024-10-30 11:47:37 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,101 @@
+[
+    {
+        "nombre": "Amanita muscaria",
+        "descripcion": "Este hongo es conocido por su característico sombrero rojo con manchas blancas, una de las especies de hongos más icónicas y fácilmente reconocibles. Crece comúnmente en bosques de coníferas y caducifolios, a menudo asociado con pinos y abedules. Aunque es venenoso si se consume crudo, ha sido utilizado en varias culturas en ceremonias tradicionales debido a sus propiedades psicoactivas. La Amanita muscaria destaca en el ecosistema como un hongo micorrízico, estableciendo relaciones simbióticas con las raíces de ciertos árboles, lo cual es fundamental para la salud del suelo y el crecimiento forestal.",
+        "fecha": "Aracuania",
+        "signo": "funga",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_010.png?raw=true",
+        "fotos": ["amanita01.webp", "amanita02.webp", "amanita03.webp"],
+        "github": "https://github.com/franirribarrat"
+
+    },
+    {
+        "nombre": "Chincol",
+        "fecha": "Aaraucania",
+        "signo": "fauna",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_09.png?raw=true",
+        "fotos": ["chincol01.webp", "chincol02.webp", "chincol02.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Copihue",
+        "fecha": "Araucania",
+        "signo": "flora",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_03.png?raw=true",
+        "fotos": ["copihue01.webp", "copihue02.webp", "copihue03.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Cola de pavo",
+        "fecha": "Aaraucania",
+        "signo": "funga",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_02.png?raw=true",
+        "fotos": ["hongo01.webp", "hongo02.webp", "hongo03.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Hongo de la risa",
+        "fecha": "Caillin",
+        "signo": "funga",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_05.png?raw=true",
+        "fotos": ["hongodelarisa01.webp", "hongodelarisa02.webp", "hongodelarisa03.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Lagartija",
+        "fecha": "Niblinto",
+        "signo": "fauna",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_06.png?raw=true",
+        "fotos": ["lagarto01.webp", "lagarto02.webp", "lagarto03.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Liquen",
+        "fecha": "Niblinto",
+        "signo": "funga",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_01.png?raw=true",
+        "fotos": ["liquen01.webp", "liquen02.webp", "liquen03.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Liquen chascon",
+        "fecha": "Niblinto",
+        "signo": "funga",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_08.png?raw=true",
+        "fotos": ["liquenchascon01.webp", "liquenchascon02.webp", "liquenchascon03.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Liquen palo",
+        "fecha": "Niblinto",
+        "signo": "funga",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_04.png?raw=true",
+        "fotos": ["liquenpalo01.webp", "liquenpalo02.webp", "liquenpalo03.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Mariposa",
+        "fecha": "Caillin",
+        "signo": "fauna",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_07.png?raw=true",
+        "fotos": ["mariposa01.webp", "mariposa02.webp", "mariposa03.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Oreja de palo",
+        "fecha": "Caillin",
+        "signo": "fuga",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_07.png?raw=true",
+        "fotos": ["orejadepalo01.webp", "orejadepalo02.webp", "orejadepalo03.webp"],
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Pololo",
+        "fecha": "Caillin",
+        "signo": "fauna",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_07.png?raw=true",
+        "fotos": ["pololo01.webp", "pololo02.webp", "pololo03.webp"],
+        "github": "https://github.com/franirribarrat"
+    }
+]
+
