8e4c6a7d078d3e873ce36c45822d3128924c1e83
Franirribarrat <franirribarrat@uc.cl>
2024-10-30 12:30:43 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -7,20 +7,19 @@
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/pamanita01.webp",
         "fotos": ["amanita01.webp", "amanita02.webp", "amanita03.webp"],
         "github": "https://github.com/franirribarrat"
-
     },
     {
         "nombre": "Chincol",
-        "descripcion":"El chincol es un ave pequeña y robusta, fácilmente identificable por su plumaje marrón y gris, con una distintiva franja negra en su cabeza y garganta. Es una especie común en la región de La Araucanía, donde habita en áreas rurales, jardines y campos abiertos. Su canto melodioso es característico de las mañanas y marca su territorio durante la época de reproducción. Adaptable y sociable, el chincol juega un papel importante en el ecosistema al controlar insectos y dispersar semillas."
+        "descripcion": "El chincol es un ave pequeña y robusta, fácilmente identificable por su plumaje marrón y gris, con una distintiva franja negra en su cabeza y garganta. Es una especie común en la región de La Araucanía, donde habita en áreas rurales, jardines y campos abiertos. Su canto melodioso es característico de las mañanas y marca su territorio durante la época de reproducción. Adaptable y sociable, el chincol juega un papel importante en el ecosistema al controlar insectos y dispersar semillas.",
         "fecha": "Araucania",
         "signo": "fauna",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/pchincol01.webp",
-        "fotos": ["chincol01.webp", "chincol02.webp", "chincol02.webp"],
+        "fotos": ["chincol01.webp", "chincol02.webp", "chincol03.webp"],
         "github": "https://github.com/franirribarrat"
     },
     {
         "nombre": "Copihue",
-        "descripcion":"El copihue, flor nacional de Chile, es una enredadera con flores en forma de campana de color rojo intenso, aunque también puede encontrarse en tonos rosados y blancos. Crece en los bosques húmedos y templados de La Araucanía, trepando árboles y arbustos con sus tallos leñosos. Su floración es un símbolo de la riqueza natural chilena, y es valorado tanto por su belleza como por su significado cultural en las tradiciones del sur de Chile."
+        "descripcion": "El copihue, flor nacional de Chile, es una enredadera con flores en forma de campana de color rojo intenso, aunque también puede encontrarse en tonos rosados y blancos. Crece en los bosques húmedos y templados de La Araucanía, trepando árboles y arbustos con sus tallos leñosos. Su floración es un símbolo de la riqueza natural chilena, y es valorado tanto por su belleza como por su significado cultural en las tradiciones del sur de Chile.",
         "fecha": "Araucania",
         "signo": "flora",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/pcopihue01.webp",
@@ -29,7 +28,7 @@
     },
     {
         "nombre": "Cola de pavo",
-        "descripcion":"Este hongo, llamado así por su parecido con la cola de un pavo, se distingue por sus bandas concéntricas de colores que van del marrón y gris al blanco. Crece en forma de abanico sobre troncos y ramas caídas en los bosques de La Araucanía, donde cumple un importante rol ecológico al descomponer materia orgánica. Además de su llamativa apariencia, el Trametes versicolor es conocido por sus propiedades medicinales, utilizadas en distintas culturas."
+        "descripcion": "Este hongo, llamado así por su parecido con la cola de un pavo, se distingue por sus bandas concéntricas de colores que van del marrón y gris al blanco. Crece en forma de abanico sobre troncos y ramas caídas en los bosques de La Araucanía, donde cumple un importante rol ecológico al descomponer materia orgánica. Además de su llamativa apariencia, el Trametes versicolor es conocido por sus propiedades medicinales, utilizadas en distintas culturas.",
         "fecha": "Araucania",
         "signo": "funga",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/phongo01.webp",
@@ -38,7 +37,7 @@
     },
     {
         "nombre": "Hongo de la risa",
-        "descripcion":"Este hongo, conocido popularmente como 'Hongo de la Risa', es de color amarillo a anaranjado y crece en grupos sobre madera en descomposición, especialmente en bosques húmedos. Su nombre proviene de sus efectos psicoactivos, que en dosis bajas pueden inducir sensaciones de euforia y risas. Sin embargo, también es tóxico, por lo que debe manejarse con precaución. El Gymnopilus luteus es una especie interesante en el estudio de los hongos con propiedades alucinógenas y se encuentra en algunas áreas de La Araucanía."
+        "descripcion": "Este hongo, conocido popularmente como 'Hongo de la Risa', es de color amarillo a anaranjado y crece en grupos sobre madera en descomposición, especialmente en bosques húmedos. Su nombre proviene de sus efectos psicoactivos, que en dosis bajas pueden inducir sensaciones de euforia y risas. Sin embargo, también es tóxico, por lo que debe manejarse con precaución. El Gymnopilus luteus es una especie interesante en el estudio de los hongos con propiedades alucinógenas y se encuentra en algunas áreas de La Araucanía.",
         "fecha": "Caillin",
         "signo": "funga",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/phongodelarisa01.webp",
@@ -47,7 +46,7 @@
     },
     {
         "nombre": "Lagartija pintada",
-        "descripcion":"La lagartija pintada es una especie endémica de Chile y Argentina, fácilmente reconocible por su cuerpo robusto y colores vibrantes que varían entre tonos verdes, amarillos y negros, formando patrones únicos en cada individuo. Habita principalmente en los bosques templados de La Araucanía, donde se refugia entre la vegetación baja y troncos caídos. Este reptil es muy activo durante el día, cazando insectos y pequeños invertebrados, y juega un papel importante en el equilibrio ecológico de su hábitat."
+        "descripcion": "La lagartija pintada es una especie endémica de Chile y Argentina, fácilmente reconocible por su cuerpo robusto y colores vibrantes que varían entre tonos verdes, amarillos y negros, formando patrones únicos en cada individuo. Habita principalmente en los bosques templados de La Araucanía, donde se refugia entre la vegetación baja y troncos caídos. Este reptil es muy activo durante el día, cazando insectos y pequeños invertebrados, y juega un papel importante en el equilibrio ecológico de su hábitat.",
         "fecha": "Niblinto",
         "signo": "fauna",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/plagarto01.webp",
@@ -56,7 +55,7 @@
     },
     {
         "nombre": "Pseudocyphelaria berberina",
-        "descripcion":"Este liquen se distingue por su forma de abanico y su coloración grisácea con tonos azulados o marrones en su superficie, mientras que su parte inferior suele ser de un color más claro. Crece en bosques húmedos y templados, típicos de regiones como La Araucanía, adherido a la corteza de árboles o en suelos ricos en nutrientes. Es sensible a la contaminación, lo que lo convierte en un importante bioindicador de la calidad ambiental en su hábitat."
+        "descripcion": "Este liquen se distingue por su forma de abanico y su coloración grisácea con tonos azulados o marrones en su superficie, mientras que su parte inferior suele ser de un color más claro. Crece en bosques húmedos y templados, típicos de regiones como La Araucanía, adherido a la corteza de árboles o en suelos ricos en nutrientes. Es sensible a la contaminación, lo que lo convierte en un importante bioindicador de la calidad ambiental en su hábitat.",
         "fecha": "Niblinto",
         "signo": "funga",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/pliquen01.webp",
@@ -65,7 +64,7 @@
     },
     {
         "nombre": "Musgo",
-        "descripcion":"Los musgos son plantas no vasculares que pertenecen al filo Bryophyta. Se caracterizan por su pequeño tamaño y su forma verde y suave, formando densas alfombras en suelos, rocas y troncos en ambientes húmedos. En La Araucanía, los musgos son comunes en los bosques, donde contribuyen a la retención de humedad y la formación de suelos. Su capacidad para absorber agua y nutrientes del ambiente los convierte en importantes aliados en la conservación de ecosistemas, proporcionando hábitat y alimento para diversos organismos."
+        "descripcion": "Los musgos son plantas no vasculares que pertenecen al filo Bryophyta. Se caracterizan por su pequeño tamaño y su forma verde y suave, formando densas alfombras en suelos, rocas y troncos en ambientes húmedos. En La Araucanía, los musgos son comunes en los bosques, donde contribuyen a la retención de humedad y la formación de suelos. Su capacidad para absorber agua y nutrientes del ambiente los convierte en importantes aliados en la conservación de ecosistemas, proporcionando hábitat y alimento para diversos organismos.",
         "fecha": "Niblinto",
         "signo": "flora",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/pliquenchascon01.webp",
@@ -74,7 +73,7 @@
     },
     {
         "nombre": "Liquen palo",
-        "descripcion":"Los líquenes son organismos únicos que resultan de la simbiosis entre un hongo y un alga o cianobacteria. Se presentan en una amplia variedad de formas y colores, desde verdes y grises hasta amarillos y naranjas, y crecen adheridos a rocas, troncos, y suelos en ambientes variados, incluyendo los bosques de La Araucanía. Son altamente resistentes y pueden sobrevivir en condiciones extremas. Además, cumplen funciones ecológicas esenciales, como proteger el suelo de la erosión y servir como indicadores de la calidad del aire."
+        "descripcion": "Los líquenes son organismos únicos que resultan de la simbiosis entre un hongo y un alga o cianobacteria. Se presentan en una amplia variedad de formas y colores, desde verdes y grises hasta amarillos y naranjas, y crecen adheridos a rocas, troncos, y suelos en ambientes variados, incluyendo los bosques de La Araucanía. Son altamente resistentes y pueden sobrevivir en condiciones extremas. Además, cumplen funciones ecológicas esenciales, como proteger el suelo de la erosión y servir como indicadores de la calidad del aire.",
         "fecha": "Niblinto",
         "signo": "funga",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/pliquenpalo01.webp",
@@ -83,7 +82,7 @@
     },
     {
         "nombre": "Mariposa de la tarde",
-        "descripcion":"La Mariposa de la Tarde, conocida por su tamaño pequeño y coloración vibrante, es una especie común en los bosques de La Araucanía. Se distingue por sus alas de tonalidades que varían entre el azul y el negro, con marcas claras que resaltan su belleza. Esta mariposa es más activa durante las horas del día y se puede observar en áreas floridas, donde se alimenta del néctar. Su ciclo de vida incluye una fase de oruga que se alimenta de varias plantas, y su presencia es un indicativo de un ecosistema saludable, contribuyendo a la polinización de diversas especies vegetales."
+        "descripcion": "La Mariposa de la Tarde, conocida por su tamaño pequeño y coloración vibrante, es una especie común en los bosques de La Araucanía. Se distingue por sus alas de tonalidades que varían entre el azul y el negro, con marcas claras que resaltan su belleza. Esta mariposa es más activa durante las horas del día y se puede observar en áreas floridas, donde se alimenta del néctar. Su ciclo de vida incluye una fase de oruga que se alimenta de varias plantas, y su presencia es un indicativo de un ecosistema saludable, contribuyendo a la polinización de diversas especies vegetales.",
         "fecha": "Caillin",
         "signo": "fauna",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/pmariposa01.webp",
@@ -92,16 +91,16 @@
     },
     {
         "nombre": "Oreja de palo",
-        "descripcion":"Conocido como oreja de palo por su forma distintiva que recuerda a una oreja humana, este hongo tiene una coloración que varía entre el marrón oscuro y el gris, con una textura gelatinosa y suave. Crece en troncos y ramas muertas, especialmente en ambientes húmedos de bosques, como los de La Araucanía. Además de su peculiar apariencia, el hongo oreja de palo es comestible y se utiliza en diversas preparaciones culinarias en varias culturas. Su presencia en el ecosistema contribuye a la descomposición de la materia orgánica, desempeñando un papel vital en el reciclaje de nutrientes en el hábitat."
+        "descripcion": "Conocido como oreja de palo por su forma distintiva que recuerda a una oreja humana, este hongo tiene una coloración que varía entre el marrón oscuro y el gris, con una textura gelatinosa y suave. Crece en troncos y ramas muertas, especialmente en ambientes húmedos de bosques, como los de La Araucanía. Además de su peculiar apariencia, el hongo oreja de palo es comestible y se utiliza en diversas preparaciones culinarias en varias culturas. Su presencia en el ecosistema contribuye a la descomposición de la materia orgánica, desempeñando un papel vital en el reciclaje de nutrientes en el hábitat.",
         "fecha": "Caillin",
-        "signo": "fuga",
+        "signo": "funga",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/orejadepalo03.webp",
         "fotos": ["orejadepalo01.webp", "orejadepalo02.webp", "orejadepalo03.webp"],
         "github": "https://github.com/franirribarrat"
     },
     {
         "nombre": "Pololo",
-        "descripcion":"El pololo es un insecto endémico de Chile, perteneciente a la familia de los gorgojos. Este pequeño insecto es conocido por su coloración marrón a negra y su forma ovalada. Generalmente, se encuentra en ambientes rurales y urbanos, donde se alimenta de materia orgánica en descomposición, como hojas y madera en descomposición. Además, es comúnmente visto en jardines y cultivos. Su ciclo de vida incluye varias etapas, desde huevo hasta larva, y juega un papel importante en la descomposición y reciclaje de nutrientes en el ecosistema, contribuyendo así a la salud del suelo."
+        "descripcion": "El pololo es un insecto endémico de Chile, perteneciente a la familia de los gorgojos. Este pequeño insecto es conocido por su coloración marrón a negra y su forma ovalada. Generalmente, se encuentra en ambientes rurales y urbanos, donde se alimenta de materia orgánica en descomposición, como hojas y madera en descomposición. Además, es comúnmente visto en jardines y cultivos. Su ciclo de vida incluye varias etapas, desde huevo hasta larva, y juega un papel importante en la descomposición y reciclaje de nutrientes en el ecosistema, contribuyendo así a la salud del suelo.",
         "fecha": "Caillin",
         "signo": "fauna",
         "foto": "https://raw.githubusercontent.com/Franirribarrat/dno096-13/refs/heads/main/img/ppololo01.webp",
@@ -109,4 +108,3 @@
         "github": "https://github.com/franirribarrat"
     }
 ]
-
